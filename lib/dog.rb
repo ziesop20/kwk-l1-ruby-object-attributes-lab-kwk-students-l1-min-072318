5dd884abf065261aba9069cc8bfc48138e579399
ziesop20 <sincerelysophie@hotmail.com>
2018-07-27 18:39:51 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,10 @@
 # dog.rb
+class Dog 
+  attr_accessor :name, :breed 
+end 
+
+dog = Dog.new 
+dog.name = "Puppy"
+dog.breed = "Golden_Retriever"
+
+# puts "My new dog is a #{"
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,8 @@
 # person.rb
+class Person 
+  attr_accessor :name, :job 
+end 
+
+person = Person.new 
+person.name = "Sophie"
+person.job = "Professional Student"
